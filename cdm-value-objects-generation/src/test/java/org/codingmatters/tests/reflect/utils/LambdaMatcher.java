a6b9706036c0667c3b1968fa3d0e571c9dd95fdb
nelt <nel.taurisson@gmail.com>
2016-09-08 22:58:03 +0000

    LamndaMatcher extends TypeSafeMatcher

--- cdm-value-objects-generation/src/test/java/org/codingmatters/tests/reflect/utils/LambdaMatcher.java
+++ cdm-value-objects-generation/src/test/java/org/codingmatters/tests/reflect/utils/LambdaMatcher.java
@@ -1,15 +1,15 @@
 package org.codingmatters.tests.reflect.utils;
 
-import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 
 /**
  * Created by nelt on 9/8/16.
  */
-public class LambdaMatcher<T> extends BaseMatcher<T> {
+public class LambdaMatcher<T> extends TypeSafeMatcher<T> {
 
     static public <T> LambdaMatcher<T> match(String description, Lambda<T> lamda) {
-        return new LambdaMatcher<T>(description, lamda);
+        return new LambdaMatcher(description, lamda);
     }
 
     private final String description;
@@ -21,13 +21,13 @@ public class LambdaMatcher<T> extends BaseMatcher<T> {
     }
 
     @Override
-    public boolean matches(Object item) {
-        return lambda.matches(item);
+    public void describeTo(Description description) {
+        description.appendText(this.description);
     }
 
     @Override
-    public void describeTo(Description description) {
-        description.appendText(this.description);
+    protected boolean matchesSafely(T item) {
+        return this.lambda.matches(item);
     }
 
     public interface Lambda<T> {
