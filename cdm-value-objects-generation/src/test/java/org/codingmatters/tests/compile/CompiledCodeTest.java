ca0bca8702eb435974411e326da8770a3f84325d
nelt <nel.taurisson@gmail.com>
2016-09-09 00:34:39 +0000

    refactored test to use temporary folder rule

--- cdm-value-objects-generation/src/test/java/org/codingmatters/tests/compile/CompiledCodeTest.java
+++ cdm-value-objects-generation/src/test/java/org/codingmatters/tests/compile/CompiledCodeTest.java
@@ -1,14 +1,13 @@
 package org.codingmatters.tests.compile;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.UUID;
 
 import static org.codingmatters.tests.reflect.ReflectMatchers.aClass;
 import static org.codingmatters.tests.reflect.ReflectMatchers.aMethod;
@@ -21,18 +20,15 @@ import static org.junit.Assert.assertThat;
 public class CompiledCodeTest {
 
     @Rule
+    public TemporaryFolder dir = new TemporaryFolder();
+    @Rule
     public ExpectedException exception = ExpectedException.none();
 
-    private File dir;
     private CompiledCode compiled;
 
     @Before
     public void setUp() throws Exception {
-        this.dir = new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
-        dir.mkdirs();
-        File packageDir = new File(this.dir, "org/codingmatters");
-        packageDir.mkdirs();
-        File helloWorld = new File(packageDir, "HelloWorld.java");
+        File helloWorld = new File(this.dir.newFolder("org", "codingmatters"), "HelloWorld.java");
 
         try(FileWriter writer = new FileWriter(helloWorld)) {
             writer.write(
@@ -51,21 +47,7 @@ public class CompiledCodeTest {
             writer.flush();
         }
 
-        this.compiled = CompiledCode.compile(this.dir);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        this.delete(this.dir);
-    }
-
-    private void delete(File file) {
-        if(file.isDirectory()) {
-            for (File child : file.listFiles()) {
-                this.delete(child);
-            }
-        }
-        file.delete();
+        this.compiled = CompiledCode.compile(this.dir.getRoot());
     }
 
     @Test
