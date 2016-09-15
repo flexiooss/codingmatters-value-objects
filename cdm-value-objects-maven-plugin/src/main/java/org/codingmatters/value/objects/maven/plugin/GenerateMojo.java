a60b6a6dbf200a854057af50ad2256118d01c02b
nelt <nel.taurisson@gmail.com>
2016-09-15 21:19:59 +0000

    [value-object] maven plugin test harness in place, demo project setup

--- cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/GenerateMojo.java
+++ cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/GenerateMojo.java
@@ -4,14 +4,32 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
 
 /**
  * Created by nelt on 9/1/16.
  */
 @Mojo(name = "generate")
 public class GenerateMojo extends AbstractMojo {
+
+    @Parameter(required = true)
+    private File inputSpecification;
+
+    @Parameter(defaultValue = "${basedir}/target/generated-sources/")
+    private File outputDirectory;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.printf("Will eventually generate:\n\t- spec: %s\n\t- output directory: %s\n", this.inputSpecification, this.outputDirectory);
+    }
 
+    public File getInputSpecification() {
+        return inputSpecification;
+    }
+
+    public File getOutputDirectory() {
+        return outputDirectory;
     }
 }
---
+++ cdm-value-objects-maven-plugin/src/test/java/org/codingmatters/value/objects/maven/plugin/GenerateMojoTest.java
@@ -0,0 +1,53 @@
+package org.codingmatters.value.objects.maven.plugin;
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Enumeration;
+
+import static org.hamcrest.Matchers.endsWith;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by nelt on 9/15/16.
+ */
+public class GenerateMojoTest {
+
+    @Rule
+    public MojoRule rule = new MojoRule();
+
+    @Test
+    public void completeConfiguration() throws Exception {
+        File pom = this.fileResource( "configuration/complete/pom.xml" );
+        GenerateMojo mojo = (GenerateMojo) rule.lookupMojo("generate", pom);
+        assertNotNull(mojo);
+
+        assertThat(mojo.getInputSpecification().getAbsolutePath(), endsWith("src/main/resources/spec.yaml"));
+        assertThat(mojo.getOutputDirectory().getAbsolutePath(), endsWith("target/generated-sources"));
+    }
+
+//    @Test
+//    public void defaultConfiguration() throws Exception {
+//        File pom = this.fileResource( "configuration/default/pom.xml" );
+//        GenerateMojo mojo = (GenerateMojo) rule.lookupMojo("generate", pom);
+//        assertNotNull(mojo);
+//
+//        assertThat(mojo.getInputSpecification(), is(nullValue()));
+//        assertThat(mojo.getOutputDirectory().getAbsolutePath(), endsWith("target/generated-sources"));
+//    }
+
+    private File fileResource(String name) throws Exception {
+        Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(name);
+        while(urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            if(url.getProtocol().equals("file")) {
+                return new File(url.toURI());
+            }
+        }
+        return null;
+    }
+}
