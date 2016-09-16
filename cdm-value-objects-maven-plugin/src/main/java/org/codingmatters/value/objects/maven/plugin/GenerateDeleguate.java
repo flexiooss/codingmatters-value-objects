fbfe3185b459c121dbc6ddbf6d06071a72da9f43
nelt <nel.taurisson@gmail.com>
2016-09-16 05:10:08 +0000

    [value-object] maven plugin dev start

--- cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/GenerateDeleguate.java
+++ cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/GenerateDeleguate.java
@@ -1,6 +1,14 @@
 package org.codingmatters.value.objects.maven.plugin;
 
+import org.codingmatters.value.objects.SpecCodeGenerator;
+import org.codingmatters.value.objects.SpecReader;
+import org.codingmatters.value.objects.exception.LowLevelSyntaxException;
+import org.codingmatters.value.objects.exception.SpecSyntaxException;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by nelt on 9/16/16.
@@ -17,6 +25,18 @@ public class GenerateDeleguate implements Runnable {
 
     @Override
     public void run() {
-
+        this.outputDirectory.mkdirs();
+        SpecReader reader = new SpecReader();
+        try {
+            try(InputStream in = new FileInputStream(this.inputSpecification)) {
+                new SpecCodeGenerator(reader.read(in), "org.generated").generateTo(this.outputDirectory);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SpecSyntaxException e) {
+            e.printStackTrace();
+        } catch (LowLevelSyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
