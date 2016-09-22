3848a6b059af5598fab49158fc731a63a0953eb2
nelt <nel.taurisson@gmail.com>
2016-09-22 05:12:28 +0000

    [value-object-generation] extracted value interface, implementation and builder

--- cdm-value-objects-generation/src/main/java/org/codingmatters/value/objects/generation/SpecCodeGenerator.java
+++ cdm-value-objects-generation/src/main/java/org/codingmatters/value/objects/generation/SpecCodeGenerator.java
@@ -38,8 +38,7 @@ public class SpecCodeGenerator {
     private void generateValueTypesTo(ValueSpec valueSpec, File packageDestination) throws IOException {
         String interfaceName = capitalizedFirst(valueSpec.name());
 
-        TypeSpec valueBuilder = this.createValueBuilder(interfaceName, valueSpec.propertySpecs());
-        TypeSpec valueInterface = this.createValueInterface(interfaceName, valueBuilder, valueSpec.propertySpecs());
+        TypeSpec valueInterface = this.createValueInterface(interfaceName, valueSpec, valueSpec.propertySpecs());
         TypeSpec valueImpl = this.createValueImplementation(interfaceName, valueSpec.propertySpecs());
 
         this.writeJavaFile(packageDestination, valueInterface);
@@ -47,7 +46,8 @@ public class SpecCodeGenerator {
     }
 
 
-    private TypeSpec createValueInterface(String interfaceName, TypeSpec valueBuilder, List<PropertySpec> propertySpecs) {
+    private TypeSpec createValueInterface(String interfaceName, ValueSpec valueSpec, List<PropertySpec> propertySpecs) {
+        TypeSpec valueBuilder = this.createValueBuilder(interfaceName, valueSpec.propertySpecs());
         ValueInterface valueInterface = new ValueInterface(propertySpecs);
 
         return TypeSpec.interfaceBuilder(interfaceName)
@@ -67,19 +67,6 @@ public class SpecCodeGenerator {
                 .addFields(valueBuilderGenerator.fields())
                 .addMethods(valueBuilderGenerator.setters())
                 .build();
-    }
-
-    static public Object [] concat(Object first, Object ... others) {
-        int size = 1;
-        if(others != null) {
-            size += others.length;
-        }
-        Object[] result = new Object[size];
-        result[0] = first;
-        for(int i = 1 ; i < result.length ; i++) {
-            result[i] = others[i-1];
-        }
-        return result;
     }
 
     private TypeSpec createValueImplementation(String interfaceName, List<PropertySpec> propertySpecs) {
@@ -104,5 +91,18 @@ public class SpecCodeGenerator {
 
     static public String capitalizedFirst(String str) {
         return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+    static public Object [] concat(Object first, Object ... others) {
+        int size = 1;
+        if(others != null) {
+            size += others.length;
+        }
+        Object[] result = new Object[size];
+        result[0] = first;
+        for(int i = 1 ; i < result.length ; i++) {
+            result[i] = others[i-1];
+        }
+        return result;
     }
 }
