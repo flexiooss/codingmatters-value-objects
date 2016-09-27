1acdb99c281bf051a8487dd6298c102afd95e8fe
nelt <nel.taurisson@gmail.com>
2016-09-27 03:25:57 +0000

    [value-object-demo] builder from value tests

---
+++ cdm-value-objects-demo/src/test/java/org/codingmatters/value/objects/demo/BuilderFromValueTest.java
@@ -0,0 +1,41 @@
+package org.codingmatters.value.objects.demo;
+
+import org.codingmatters.value.objects.demo.referenced.ReferencedValue;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by nelt on 9/27/16.
+ */
+public class BuilderFromValueTest {
+
+    @Test
+    public void simpleValue() throws Exception {
+        Value value = Value.Builder.builder()
+                .stringProperty("toto")
+                .booleanProperty(true)
+                .build();
+
+        assertThat(Value.Builder.from(value).build(), is(value));
+    }
+
+    @Test
+    public void complexValue() throws Exception {
+        ComplexValue value = ComplexValue.Builder.builder()
+                .recursiveProperty(ComplexValue.Builder.builder())
+                .inSpecProperty(Value.Builder.builder().stringProperty("toto"))
+                .build();
+
+        assertThat(ComplexValue.Builder.from(value).build(), is(value));
+    }
+
+    @Test
+    public void externalValue() throws Exception {
+        ComplexValue value = ComplexValue.Builder.builder().outSpecProperty(ReferencedValue.Builder.builder()).build();
+
+        assertThat(ComplexValue.Builder.from(value).build(), is(value));
+    }
+
+}
