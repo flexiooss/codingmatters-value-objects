268f68fc92c22fb32c9fcda5b9be95dc89d39254
nelt <nel.taurisson@gmail.com>
2016-10-02 20:10:18 +0000

    [value-object-generation] deploy to maven central - env var name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P deploy-to-ossrh --settings .travis/travis-settings.xml
+    mvn deploy -P deploy-to-ossrh -DskipTests --settings .travis/travis-settings.xml
 fi
