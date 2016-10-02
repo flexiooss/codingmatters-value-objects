6a91029590af262c80268beba2486c2418083210
nelt <nel.taurisson@gmail.com>
2016-10-02 20:02:18 +0000

    [value-object-generation] deploy to maven central - codesigning.asc

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_0165d7629ef9_key -iv $encrypted_0165d7629ef9_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import signingkey.asc
+    openssl aes-256-cbc -K $encrypted_0165d7629ef9_key -iv $encrypted_0165d7629ef9_iv -in .travis/codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import codesigning.asc
 fi
