426d51b46228aa0740e20f8b306bb35cd677bd20
nelt <nel.taurisson@gmail.com>
2016-10-11 05:15:51 +0000

    start implementation of list interface

---
+++ cdm-value-objects-generation/src/main/java/org/codingmatters/value/objects/generation/collection/ValueList.java
@@ -0,0 +1,37 @@
+package org.codingmatters.value.objects.generation.collection;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.util.Collection;
+
+/**
+ * Created by nelt on 10/11/16.
+ */
+public class ValueList {
+
+    private final String packageName;
+
+    public ValueList(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public TypeSpec type() {
+        return TypeSpec.interfaceBuilder("ImmutableList")
+                .addModifiers(Modifier.PUBLIC)
+                .addMethod(MethodSpec.methodBuilder("contains")
+                        .addModifiers(Modifier.ABSTRACT, Modifier.PUBLIC)
+                        .addParameter(TypeName.OBJECT, "o")
+                        .returns(TypeName.BOOLEAN)
+                        .build())
+                .addMethod(MethodSpec.methodBuilder("containsAll")
+                        .addModifiers(Modifier.ABSTRACT, Modifier.PUBLIC)
+                        .addParameter(ClassName.get(Collection.class), "c")
+                        .returns(TypeName.BOOLEAN)
+                        .build())
+                .build();
+    }
+}
---
+++ cdm-value-objects-generation/src/test/java/org/codingmatters/value/objects/generation/collection/ValueListTest.java
@@ -0,0 +1,46 @@
+package org.codingmatters.value.objects.generation.collection;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+import org.codingmatters.tests.compile.CompiledCode;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.util.Collection;
+
+import static org.codingmatters.tests.reflect.ReflectMatchers.aMethod;
+import static org.codingmatters.tests.reflect.ReflectMatchers.anInterface;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by nelt on 10/11/16.
+ */
+public class ValueListTest {
+
+    @Rule
+    public TemporaryFolder dir = new TemporaryFolder();
+
+    private CompiledCode compiled(String packageName, TypeSpec type) throws Exception {
+        File dest = this.dir.newFolder();
+        JavaFile file = JavaFile.builder(packageName, type).build();
+        file.writeTo(dest);
+
+        return CompiledCode.compile(dest);
+    }
+
+
+    @Test
+    public void name() throws Exception {
+        String packageName = "org.generated";
+        CompiledCode compiled = this.compiled(packageName, new ValueList(packageName).type());
+
+        assertThat(compiled.getClass("org.generated.ImmutableList"), is(
+                anInterface()
+                        .with(aMethod().named("contains").withParameters(Object.class).returning(boolean.class))
+                        .with(aMethod().named("containsAll").withParameters(Collection.class).returning(boolean.class))
+        ));
+    }
+}
