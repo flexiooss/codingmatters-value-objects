bb7c92e97e4a061054f083d1a55e50ac8ecd122a
nelt <nel.taurisson@gmail.com>
2017-03-25 22:00:06 +0000

    [json explore] simple and simple array value

--- cdm-value-objects-json/src/test/java/org/codingmatters/value/objects/json/ExploreTest.java
+++ cdm-value-objects-json/src/test/java/org/codingmatters/value/objects/json/ExploreTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.LinkedList;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -22,19 +23,21 @@ public class ExploreTest {
     @Test
     public void simpleValueWrite() throws Exception {
         assertThat(
-                this.writeValueAsString(SimpleValue.Builder.builder()
+                this.writeValue(SimpleValue.Builder.builder()
                         .prop("a value")
+                        .listProp("a", "b", "c")
                         .build()),
-                is("{\"prop\":\"a value\"}")
+                is("{\"prop\":\"a value\",\"listProp\":[\"a\",\"b\",\"c\"]}")
         );
     }
 
     @Test
     public void simpleReadValue() throws Exception {
         assertThat(
-                this.readValue("{\"prop\":\"a value\"}"),
+                this.readValue("{\"prop\":\"a value\",\"listProp\":[\"a\",\"b\",\"c\"]}"),
                 is(SimpleValue.Builder.builder()
                         .prop("a value")
+                        .listProp("a", "b", "c")
                         .build())
         );
     }
@@ -42,33 +45,83 @@ public class ExploreTest {
 
     private JsonFactory factory = new JsonFactory();
 
-    private String writeValueAsString(SimpleValue value) throws IOException {
-        try(
-                OutputStream out = new ByteArrayOutputStream()) {
-
+    private String writeValue(SimpleValue value) throws IOException {
+        try(OutputStream out = new ByteArrayOutputStream()) {
             JsonGenerator generator = this.factory.createGenerator(out);
             generator.writeStartObject();
-            generator.writeStringField("prop", value.prop());
+
+            generator.writeFieldName("prop");
+            this.writeSimpleValue(value, generator);
+
+            generator.writeFieldName("listProp");
+            this.writeSimpleArray(value, generator);
+
             generator.writeEndObject();
             generator.close();
             return out.toString();
         }
     }
 
+    private void writeSimpleValue(SimpleValue value, JsonGenerator generator) throws IOException {
+        generator.writeString(value.prop());
+    }
+
+    private void writeSimpleArray(SimpleValue value, JsonGenerator generator) throws IOException {
+        generator.writeStartArray();
+        for (String elmt : value.listProp()) {
+            generator.writeString(elmt);
+        }
+        generator.writeEndArray();
+    }
+
     private SimpleValue readValue(String json) throws IOException {
         try(JsonParser parser = this.factory.createParser(json.getBytes())) {
             SimpleValue.Builder builder = SimpleValue.Builder.builder();
             if (parser.nextToken() != JsonToken.START_OBJECT) {
-                throw new IOException("Expected data to start with an Object");
+                throw new IOException(
+                        String.format("reading a %s object, was expecting %s, but was %s",
+                                SimpleValue.class.getName(), JsonToken.START_ARRAY, parser.currentToken()
+                        )
+                );
             }
             while (parser.nextToken() != JsonToken.END_OBJECT) {
                 String fieldName = parser.getCurrentName();
                 if("prop".equals(fieldName)) {
-                    parser.nextToken();
-                    builder.prop(parser.getText());
+                    this.readSimpleProperty(parser, builder);
+                } else if("listProp".equals(fieldName)) {
+                    this.readSimpleArrayValue(parser, builder);
                 }
             }
             return builder.build();
+        }
+    }
+
+    private void readSimpleProperty(JsonParser parser, SimpleValue.Builder builder) throws IOException {
+        parser.nextToken();
+        if(parser.currentToken() == JsonToken.VALUE_STRING) {
+            builder.prop(parser.getText());
+        } else if(parser.currentToken() == JsonToken.VALUE_NULL) {
+            builder.prop(null);
+        }
+    }
+
+    private void readSimpleArrayValue(JsonParser parser, SimpleValue.Builder builder) throws IOException {
+        parser.nextToken();
+        if(parser.currentToken() == JsonToken.VALUE_NULL) {
+            builder.listProp();
+        } else {
+            if(parser.currentToken() != JsonToken.START_ARRAY) {
+                throw new IOException(
+                        String.format("reading property %s, was expecting %s, but was %s",
+                                "listProp", JsonToken.START_ARRAY, parser.currentToken()
+                        )
+                );
+            }
+            LinkedList<String> listValue = new LinkedList<>();
+            while(parser.nextToken() != JsonToken.END_ARRAY) {
+                listValue.add(parser.getText());
+            }
+            builder.listProp(listValue);
         }
     }
 }
