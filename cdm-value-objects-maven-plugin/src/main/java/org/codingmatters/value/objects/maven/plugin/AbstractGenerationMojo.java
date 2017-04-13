8aa08cda56c1bff4bf3ee0745cb2c7161958b925
nelt <nel.taurisson@gmail.com>
2017-04-13 04:47:35 +0000

    [maven plugin] abstracted configuration to abstract mojo

---
+++ cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/AbstractGenerationMojo.java
@@ -0,0 +1,33 @@
+package org.codingmatters.value.objects.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+
+/**
+ * Created by nelt on 4/13/17.
+ */
+public abstract class AbstractGenerationMojo extends AbstractMojo {
+
+    @Parameter(required = true)
+    private String destinationPackage;
+
+    @Parameter(required = true)
+    private File inputSpecification;
+
+    @Parameter(defaultValue = "${basedir}/target/generated-sources/")
+    private File outputDirectory;
+
+    public File getInputSpecification() {
+        return inputSpecification;
+    }
+
+    public File getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public String getDestinationPackage() {
+        return this.destinationPackage;
+    }
+}
--- cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/GenerateMojo.java
+++ cdm-value-objects-maven-plugin/src/main/java/org/codingmatters/value/objects/maven/plugin/GenerateMojo.java
@@ -1,56 +1,32 @@
 package org.codingmatters.value.objects.maven.plugin;
 
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.codingmatters.value.objects.exception.LowLevelSyntaxException;
 import org.codingmatters.value.objects.exception.SpecSyntaxException;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
  * Created by nelt on 9/1/16.
  */
 @Mojo(name = "generate")
-public class GenerateMojo extends AbstractMojo {
-
-    @Parameter(required = true)
-    private String destinationPackage;
-
-    @Parameter(required = true)
-    private File inputSpecification;
-
-    @Parameter(defaultValue = "${basedir}/target/generated-sources/")
-    private File outputDirectory;
+public class GenerateMojo extends AbstractGenerationMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         this.getLog().info("generating value object with configuration:");
-        this.getLog().info("\t- to package          :" + this.destinationPackage);
-        this.getLog().info("\t- specification file  :" + this.inputSpecification.getAbsolutePath());
-        this.getLog().info("\t- to output directory : " + this.outputDirectory.getAbsolutePath());
+        this.getLog().info("\t- to package          :" + this.getDestinationPackage());
+        this.getLog().info("\t- specification file  :" + this.getInputSpecification().getAbsolutePath());
+        this.getLog().info("\t- to output directory : " + this.getOutputDirectory().getAbsolutePath());
 
         try {
-            new GenerateDeleguate(this.destinationPackage, this.inputSpecification, this.outputDirectory).run();
+            new GenerateDeleguate(this.getDestinationPackage(), this.getInputSpecification(), this.getOutputDirectory()).run();
         } catch (SpecSyntaxException | LowLevelSyntaxException e) {
-            throw new MojoFailureException("unparseable specification file : " + this.inputSpecification.getAbsolutePath(), e);
+            throw new MojoFailureException("unparseable specification file : " + this.getInputSpecification().getAbsolutePath(), e);
         } catch (IOException e) {
-            throw new MojoExecutionException("something went wrong whie generating value objects at " + this.destinationPackage, e);
+            throw new MojoExecutionException("something went wrong whie generating value objects at " + this.getDestinationPackage(), e);
         }
-    }
-
-    public File getInputSpecification() {
-        return inputSpecification;
-    }
-
-    public File getOutputDirectory() {
-        return outputDirectory;
-    }
-
-    public String getDestinationPackage() {
-        return this.destinationPackage;
     }
 }
