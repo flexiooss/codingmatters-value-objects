83dfedfbe3725d0cc078924224552886ac512753
Nel <nel@flexio.fr>
2018-03-19 23:18:54 +0100

    optional object value

--- cdm-value-objects-values/src/main/java/org/codingmatters/value/objects/values/optional/OptionalObjectValue.java
+++ cdm-value-objects-values/src/main/java/org/codingmatters/value/objects/values/optional/OptionalObjectValue.java
@@ -4,6 +4,10 @@ import org.codingmatters.value.objects.values.ObjectValue;
 import org.codingmatters.value.objects.values.PropertyValue;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class OptionalObjectValue {
 
@@ -11,24 +15,62 @@ public class OptionalObjectValue {
         return new OptionalObjectValue(value);
     }
 
-    private final ObjectValue value;
+    private final Optional<ObjectValue> value;
 
     public OptionalObjectValue(ObjectValue value) {
-        this.value = value;
+        this.value = Optional.ofNullable(value);
     }
 
+
     public boolean has(String property) {
-        return this.value.has(property);
+        return this.value.isPresent() && this.value.get().has(property);
     }
     public String [] propertyNames() {
-        return this.value.propertyNames();
+        return this.value.isPresent() ? this.value.get().propertyNames() : new String[0];
     }
 
     public Optional<PropertyValue> property(String property) {
         if(this.has(property)) {
-            return Optional.of(this.value.property(property));
+            return Optional.of(this.value.get().property(property));
         } else {
             return Optional.empty();
         }
+    }
+
+
+    public ObjectValue get() {
+        return value.get();
+    }
+
+    public boolean isPresent() {
+        return value.isPresent();
+    }
+
+    public void ifPresent(Consumer<? super ObjectValue> consumer) {
+        value.ifPresent(consumer);
+    }
+
+    public Optional<ObjectValue> filter(Predicate<? super ObjectValue> predicate) {
+        return value.filter(predicate);
+    }
+
+    public <U> Optional<U> map(Function<? super ObjectValue, ? extends U> function) {
+        return value.map(function);
+    }
+
+    public <U> Optional<U> flatMap(Function<? super ObjectValue, Optional<U>> function) {
+        return value.flatMap(function);
+    }
+
+    public ObjectValue orElse(ObjectValue objectValue) {
+        return value.orElse(objectValue);
+    }
+
+    public ObjectValue orElseGet(Supplier<? extends ObjectValue> supplier) {
+        return value.orElseGet(supplier);
+    }
+
+    public <X extends Throwable> ObjectValue orElseThrow(Supplier<? extends X> supplier) throws X {
+        return value.orElseThrow(supplier);
     }
 }
