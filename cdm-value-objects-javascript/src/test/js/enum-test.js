05151ba7cb480320cf285341cfea09c4d1c8a6e2
nicolas@flexio.fr <nicolas@flexio.fr>
2019-01-18 15:09:01 +0100

    Remove javascript module

--- cdm-value-objects-javascript/src/test/js/enum-test.js
+++
@@ -1,5 +0,0 @@
-import {InSpecEnumPropertiesSingle} from '../org/generated/inspecenumproperties/InSpecEnumPropertiesSingle'
-
-test('test equality', () => {
-  expect(InSpecEnumPropertiesSingle.SA).toBe(InSpecEnumPropertiesSingle.SA);
-});
