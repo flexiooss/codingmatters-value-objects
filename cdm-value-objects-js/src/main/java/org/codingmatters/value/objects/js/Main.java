4c686a0f103fb2827bbc4f4187c5a3aeaaf6ae65
nicolas@flexio.fr <nicolas@flexio.fr>
2019-03-27 15:23:47 +0100

    Add file extension

--- cdm-value-objects-js/src/main/java/org/codingmatters/value/objects/js/Main.java
+++ cdm-value-objects-js/src/main/java/org/codingmatters/value/objects/js/Main.java
@@ -9,40 +9,53 @@ import org.codingmatters.value.objects.js.parser.model.ParsedYAMLSpec;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Main {
 
     public static void main( String[] args ) throws ProcessingException, GenerationException {
-//        args = new String[]{
-//                "/home/nico/workspace/codingmatters-value-objects/cdm-value-objects-js/target/js-test/04_objectWithEmbeddedValueSpec.yaml",
-//                "/home/nico/workspace/codingmatters-value-objects/cdm-value-objects-js/target/js-test",
-//                "org.generated",
-//                "--no-sub-package"
-//        };
-
         if( args.length < 3 || args[2].isEmpty() ){
             System.out.println( "Args: <yaml spec file path> <target directory> <root package> [--no-sub-package]" );
             System.out.println( "    <yaml spec file path>: Yaml file path OR directory. If dir, then all yaml files in this dir will be generated in the same package" );
             System.out.println( "    <target directory>: generation target directory" );
             System.out.println( "    <root package>: The root package of sources" );
+            System.out.println( "    [file extension]: Optional yaml file extension to use ( default: *.yaml *.yml )" );
             System.out.println( "    --no-sub-package Optional argument to prevent multiple yaml file to be generated in different subpackage" );
             System.exit( 1 );
         }
         String specFilePath = args[0];
         String targetDir = args[1];
         String rootPackage = args[2];
-        boolean deactivateSubPAckage = args.length >= 4 && "--no-sub-package".equals( args[3] );
-        System.out.println( "Processing generation of " + specFilePath + " in " + targetDir + " with root package " + rootPackage );
 
+        boolean deactivateSubPackage = false;
+        String[] extension = { ".yaml", ".yml" };
+        if( args.length == 4 ){
+            if( "--no-sub-package".equals( args[3] ) ){
+                deactivateSubPackage = true;
+            } else {
+                extension = args[3].split( "," );
+            }
+        }
+        if( args.length == 5 ){
+            extension = args[3].split( "," );
+            if( "--no-sub-package".equals( args[4] ) ){
+                deactivateSubPackage = true;
+            }
+        }
+
+        System.out.println( "Processing generation of " + specFilePath + " in " + targetDir + " with root package " + rootPackage );
+        System.out.println( "Included extensions: " + String.join( " ", extension ) );
         File specFile = new File( specFilePath );
         PackageFilesBuilder packageBuilder = new PackageFilesBuilder();
-        if( specFile.exists() ){
+        if( specFile.exists() )
+
+        {
             if( specFile.isDirectory() ){
                 for( File file : specFile.listFiles() ){
-                    if( file.getName().endsWith( ".yaml" ) || file.getName().endsWith( ".yml" ) ){
+                    if( Arrays.stream( extension ).anyMatch( ext->file.getName().endsWith( ext ) ) ){
                         String targetRootPackage = rootPackage;
-                        if( !deactivateSubPAckage ){
+                        if( !deactivateSubPackage ){
                             targetRootPackage += "." + file.getName().substring( 0, file.getName().lastIndexOf( "." ) ).toLowerCase( Locale.ENGLISH );
                         }
                         generateSpec( targetDir, targetRootPackage, file, packageBuilder );
@@ -54,6 +67,7 @@ public class Main {
             PackageFilesGenerator packageFilesGenerator = new PackageFilesGenerator( packageBuilder, targetDir );
             packageFilesGenerator.generateFiles();
         }
+
     }
 
     private static void generateSpec( String targetDir, String rootPackage, File specFile, PackageFilesBuilder packageBuilder ) throws ProcessingException {
--- cdm-value-objects-js/src/main/resources/run.sh
+++ cdm-value-objects-js/src/main/resources/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -cp "lib/*" org.codingmatters.value.objects.js.Main $@
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+java -cp "$SCRIPT_DIR/lib/*" org.codingmatters.value.objects.js.Main $@
