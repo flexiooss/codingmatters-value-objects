81d09e74c515cd18e58ef142865089e055e0a82c
nico <nicolas@flexio.fr>
2020-12-01 12:12:38 +0100

    Ignore JS Test...

--- cdm-value-objects-js/src/test/java/org/codingmatters/value/objects/js/RunJsTest.java
+++ cdm-value-objects-js/src/test/java/org/codingmatters/value/objects/js/RunJsTest.java
@@ -51,7 +51,7 @@ public class RunJsTest {
         }
     }
 
-//    @Ignore
+    @Ignore
     @Test
     public void whenName_then() throws Exception {
         String dir = System.getProperty( "project.build.directory" ) + "/js-test";
