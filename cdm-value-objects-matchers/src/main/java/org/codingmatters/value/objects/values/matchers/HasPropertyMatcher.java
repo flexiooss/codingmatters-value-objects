006539af96a680585980c302d6eb9c5c381437b3
Antoine Bonneau <antoine.bonneau@flexio.fr>
2022-02-01 14:52:01 +0100

    Introduce HasProperty Matcher
    ref #120

---
+++ cdm-value-objects-matchers/src/main/java/org/codingmatters/value/objects/values/matchers/HasPropertyMatcher.java
@@ -0,0 +1,29 @@
+package org.codingmatters.value.objects.values.matchers;
+
+import org.codingmatters.value.objects.values.ObjectValue;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+
+public class HasPropertyMatcher extends TypeSafeMatcher<ObjectValue> {
+    private final String property;
+
+    public HasPropertyMatcher(String property) {
+        this.property = property;
+    }
+
+    @Override
+    protected boolean matchesSafely(ObjectValue value) {
+        return value.has(property) && ! value.property(property).isNullValue();
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("no ").appendValue(property).appendText(" property");
+    }
+
+    public static Matcher<ObjectValue> hasProperty(String property) {
+        return new HasPropertyMatcher(property);
+    }
+}
